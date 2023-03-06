02f79a1c01cef8dc0037c55c734d73839e16e287
kakao <coolofficials@gmail.com>
2023-03-06 11:05:48 +0900

    test: can_allocate test

--- test_model.py
+++ test_model.py
@@ -23,15 +23,31 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    large_batch = Batch("batch_1", "SMALL-TABLE", 20, eta=today)
+    small_order_line = OrderLine("order_1", "SMALL-TABLE", 2)
+
+    assert large_batch.can_allocate(small_order_line)
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    small_batch = Batch("batch_1", "SMALL-TABLE", 2, eta=today)
+    large_order_line = OrderLine("order_1", "SMALL-TABLE", 5)
+
+    assert small_batch.can_allocate(large_order_line) is False
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    batch = Batch("batch_1", "SMALL-TABLE", 2, eta=today)
+    order_line = OrderLine("order_1", "SMALL-TABLE", 2)
+
+    assert batch.can_allocate(order_line)
+
+
+def test_cannot_allocate_if_skus_do_not_match():
+    small_table_batch = Batch("batch_1", "SMALL-TABLE", 2, eta=today)
+    large_table_order_line = OrderLine("order_1", "LARGE-TABLE", 2)
+
+    assert small_table_batch.can_allocate(large_table_order_line) is False
 
 
 def test_prefers_warehouse_batches_to_shipments():
